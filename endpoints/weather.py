27afdcf61c23affe2e87dcbe02711c4b68b0eed4
crbazevedo <carlos.azevedo@bain.com>
2023-07-09 20:20:49 -0300

    adding services folder and refactor weather endpoint

--- endpoints/weather.py
+++ endpoints/weather.py
@@ -1,30 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from security.transport import HideSensitiveTransport
-import httpx
-import os
-import logging
+from fastapi import APIRouter
+from services.openweathermap import get_weather
 
 router = APIRouter()
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-@router.get("/weather/{city}")
-async def get_weather(city: str):
-    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
-    url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": city,
-        "appid": api_key
-    }
-    transport = HideSensitiveTransport(sensitive_params=['appid'])
-
-    async with httpx.AsyncClient(transport=transport) as client:
-        try:
-            resp = await client.request("GET", url, params=params)
-            resp.raise_for_status()
-        except httpx.HTTPStatusError as exc:
-            logger.error(f"Error fetching weather: {exc}")
-            raise HTTPException(status_code=exc.response.status_code, detail=exc.response.text)
-
-        return resp.json()
+@router.get('/{city}')
+async def weather(city: str):
+    return await get_weather(city)
