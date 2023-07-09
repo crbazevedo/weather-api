5da1eb2dc23116b37c06a8bf904914bf0a7bf3d7
crbazevedo <carlos.azevedo@bain.com>
2023-07-09 20:10:33 -0300

    adding weather endpoint

---
+++ endpoints/weather.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, HTTPException
+from security.transport import HideSensitiveTransport
+import httpx
+import os
+import logging
+
+router = APIRouter()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@router.get("/weather/{city}")
+async def get_weather(city: str):
+    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+    url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key
+    }
+    transport = HideSensitiveTransport(sensitive_params=['appid'])
+
+    async with httpx.AsyncClient(transport=transport) as client:
+        try:
+            resp = await client.request("GET", url, params=params)
+            resp.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            logger.error(f"Error fetching weather: {exc}")
+            raise HTTPException(status_code=exc.response.status_code, detail=exc.response.text)
+
+        return resp.json()
